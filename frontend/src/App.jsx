1c9792f0a2565a07fd727f614144b077e6e85c37
Nguyen Hoang Khanh Dang <dangnguyen332006@gmail.com>
2026-03-23 21:08:08 +0700

    Giao dien giao dich

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Checkout from "./components/Checkout";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Demo Đồ Án Mật Mã Học</h1>
+      <Checkout />
+    </div>
+  );
+}
+
+export default App;
