18dfd294d75760ba0b4cb361487d1973a85f7bd8
Ngo Xuan Minh Hoang <hoangminh13022006@gmail.com>
2026-04-11 17:20:44 +0700

    fix

--- services/payment_orchestrator/start.sh
+++ services/payment_orchestrator/start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+set -e
+
 # Nếu chưa có thư mục vendor, mượn một container rác để cài Composer
 if [ ! -d "vendor" ]; then
     echo "Không có vendor! Đang tải Composer..."
     docker run --rm \
-        -v $(pwd):/var/www/html \
-        -w /var/www/html \
-        laravelsail/php85-composer:latest \
+        -u "$(id -u):$(id -g)" \
+        -v "$(pwd):/app" \
+        -w /app \
+        composer:latest \
         composer install --ignore-platform-reqs
     echo "Đã tải xong vendor!"
 fi
